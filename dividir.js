6df70694b600f3a7a5814426821d59c117951e18
Martin Araneda <martin.araneda@outlook.com>
2022-11-08 10:57:29 -0300

    Agrego opcion dividir

---
+++ dividir.js
@@ -0,0 +1,9 @@
+function dividir (num1, num2){
+    if (num1 !== 0 || num2!== 0) {
+        return "No se puede dividir por 0"
+        
+    }
+    return num1 / num2
+}
+
+module.exports = dividir
